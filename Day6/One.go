cf358e437de1b3e4db51c583d17d0a52ed93e0d3
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2022-12-06 16:50:00 -0600

    Did day 6

--- Day6/One.go
+++ Day6/One.go
@@ -1,1 +1,49 @@
-package Day6
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+	"fmt"
+)
+
+func main() {
+	file, err := os.Open("DaySix.txt")
+
+	if err != nil {
+		log.Fatalf("failed to open")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	s := make([]string, 0)
+
+	for scanner.Scan() {
+		s = strings.Split(scanner.Text(), "")
+	}
+
+	m := map[string] int{
+		"c" : 2,
+		"n": 1,
+		"z": 1,
+	}
+
+	for i := 4; i < len(s); i++ {
+		m[s[i]]++
+
+		m[s[i - 4]]--
+
+		if m[s[i - 4]] == 0 {
+			delete(m, s[i - 4])
+		}
+
+		if len(m) == 4 {
+			fmt.Println(i+1)
+			break
+		}
+	}
+
+	file.Close()
+}
--- Day6/Two.go
+++ Day6/Two.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+	"fmt"
+)
+
+func main() {
+	file, err := os.Open("DaySix.txt")
+
+	if err != nil {
+		log.Fatalf("failed to open")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	s := make([]string, 0)
+
+	for scanner.Scan() {
+		s = strings.Split(scanner.Text(), "")
+	}
+
+	n := 14
+
+	m := map[string] int{}
+
+	for i := 0; i < n; i++ {
+		m[s[i]]++
+	}
+
+	for i := n; i < len(s); i++ {
+		m[s[i]]++
+
+		m[s[i - n]]--
+
+		if m[s[i - n]] == 0 {
+			delete(m, s[i - n])
+		}
+
+		if len(m) == n {
+			fmt.Println(i+1)
+			break
+		}
+	}
+
+	file.Close()
+}
